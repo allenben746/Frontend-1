c35d7297bb74b41b3e06cb03bf15da7fcda61ee3
Ben <allenben@protonmail.com>
2019-08-26 17:14:44 -0500

    Reached server response

--- frontend/src/components/SignUp.js
+++ frontend/src/components/SignUp.js
@@ -1,7 +1,6 @@
-import React, { useState, useEffect } from 'react';
-import { Link } from "react-router-dom";
+import React from 'react';
 import { withFormik, Form, Field } from "formik";
-// import axios from 'axios';
+import axios from 'axios';
 import * as Yup from "yup";
 
 // send back -> first_name, last_name, email, username
@@ -11,14 +10,12 @@ const SignUpForm = ({ errors, touched, values, status }) => {
         <div className="userForm">
             <h1>User Sign-Up</h1>
             <Form>
-                <Field type="text" name="name" placeholder="Name" />
-                {touched.name && errors.name && (
-                    <p className='errorMessage'>{errors.name}</p>
-                )}
+                <Field type="text" name="first_name" placeholder="First name" />
+                <Field type="text" name="last_name" placeholder="Last name" />
                 <Field type="text" name="email" placeholder="Email" />
                 <Field type="password" name="password" placeholder="Password" />
-                <label> I agree to the Terms of Service
-                <Field type="checkbox" name="termsOfService" />
+                <label>Bidder
+                <Field type="checkbox" name="user_classification" />
                 </label>
                 <button type="submit">Submit Info</button>
             </Form>
@@ -29,34 +26,42 @@ const SignUpForm = ({ errors, touched, values, status }) => {
 const FormikUserForm = withFormik({
     
     //Handles State
-    // mapPropsToValues({name, email, password}){
-    //     return {
-    //         name: name || "",
-    //         email: email || "",
-    //         password: password || "",
-    //     }
-    // },
-    //Yup Validation
+    mapPropsToValues({first_name, last_name, email, password, user_classification}){
+        return {
+            first_name: first_name || "",
+            last_name: last_name || "",
+            email: email || "",
+            password: password || "",
+            user_classification: user_classification || ""
+        }
+    },
+    // Yup Validation
     validationSchema: Yup.object().shape({
-        name: Yup.string()
-        .required('Name is required'),
+        first_name: Yup.string()
+        .required('First name is required'),
+        last_name: Yup.string()
+        .required('Last name is required'),
         email: Yup.string()
           .email()
           .required('Email is required'),
         password: Yup.string()
           .min(6)
           .max(20)
-          .required('A password is required')
-      })
+          .required('A password is required'),
+          user_classification: Yup.boolean()
+          .required('Checkbox is required')
+      }),
      //Post request
-    // handleSubmit(values, {setStatus, resetForm}){
-    //     console.log("Form submitted", values);
-    //     axios.post("https://reqres.in/api/users", values)
-    //     .then(res => {
-    //         setStatus(res.data)
-    //         resetForm();
-    //         })
-    //     .catch(err => console.log(err))
-    // }
+    handleSubmit(values, {setStatus, resetForm}){
+        console.log("Form submitted", values);
+        axios.post("https://silent-auction-api.herokuapp.com/api/auth/register", values)
+        .then(res => {
+            setStatus(res.data)
+            resetForm();
+            console.log("Server response ->", res.data);
+            console.log("Submitted");
+            })
+        .catch(err => console.log(err))
+    }
 })(SignUpForm);
 export default FormikUserForm;
